7d540b3fa5e5562fc55e4bfbb4cb0a9b6ae68421
CyranoRock <amoragues@itba.edu.ar>
2018-08-20 16:53:00 -0300

    add moving ship and walls

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	public float moveSpeed = 10.0f;
+	public Rigidbody2D player;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate()
+	{
+		MovePlayer();
+	}
+
+	public void MovePlayer()
+	{
+		player.velocity = new Vector2(Input.GetAxis("Horizontal"), 0.0f) * moveSpeed;
+	}
+}
