15bfae40b42de0d146daaddbf83b6d605ccc4ec9
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-08-20 22:39:43 -0300

    Add shot capabilities

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,31 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+    public float moveSpeed = 10.0f;
+    public Rigidbody2D player;
 
-	public float moveSpeed = 10.0f;
-	public Rigidbody2D player;
-	
-	// Use this for initialization
-	void Start ()
-	{
-		player.GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject ShotPrefab;
 
-	void FixedUpdate()
-	{
-		MovePlayer();
-	}
+    // Use this for initialization
+    private void Start() {
+        player.GetComponent<Rigidbody2D>();
+    }
 
-	public void MovePlayer()
-	{
-		player.velocity = new Vector2(Input.GetAxis("Horizontal"), 0.0f) * moveSpeed;
-	}
+    private void FixedUpdate() {
+        CheckMove();
+        CheckShoot();
+    }
+
+    private void CheckMove() {
+        player.velocity = new Vector2(Input.GetAxis("Horizontal"), 0.0f) * moveSpeed;
+    }
+
+    private void CheckShoot() {
+        if (!Input.GetKeyDown(KeyCode.Space)) {
+            return;
+        }
+        var shot = Instantiate(ShotPrefab);
+        shot.transform.position = transform.position;
+    }
 }
---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Shot : MonoBehaviour {
+    public Rigidbody2D body;
+    public float speed = 20.0f;
+
+
+    private void Start() {
+        body.velocity = new Vector2(0, speed);
+    }
+
+    private void Update() {
+        // Do nothing
+    }
+}
