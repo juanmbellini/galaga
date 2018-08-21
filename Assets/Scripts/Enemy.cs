929533588b31b9e3781fc269f67cac87a595101f
Facundo Gonzalez Fernandez <fgonzalez@itba.edu.ar>
2018-08-20 23:39:34 -0300

    Add enemy prefab, script and timer for shots

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public Rigidbody2D body;
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {		
+	}
+
+	private void OnCollisionEnter (Collision col)
+	{
+		Debug.Log("Choque");
+		if(col.gameObject.tag == "shot")
+		{
+			Destroy(col.gameObject);
+		}
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float moveSpeed = 10.0f;
     public Rigidbody2D player;
+    private System.DateTime lastShootTime;
+   
 
     public GameObject ShotPrefab;
 
@@ -19,12 +22,17 @@ public class Player : MonoBehaviour {
     private void CheckMove() {
         player.velocity = new Vector2(Input.GetAxis("Horizontal"), 0.0f) * moveSpeed;
     }
+    
 
-    private void CheckShoot() {
+    private void CheckShoot() {        
         if (!Input.GetKeyDown(KeyCode.Space)) {
             return;
         }
+        var ts = System.DateTime.Now - lastShootTime;
+        if (!(ts.TotalMilliseconds > 300.0f)) return;
+        lastShootTime = System.DateTime.Now;
         var shot = Instantiate(ShotPrefab);
         shot.transform.position = transform.position;
+        
     }
 }
