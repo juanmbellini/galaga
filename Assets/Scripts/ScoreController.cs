72b590a4a68acec5d49a0c2d933bace8557a8a11
Facundo Gonzalez Fernandez <fgonzalez@itba.edu.ar>
2018-08-29 20:43:15 -0300

    Score UI

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+
+	public TextMeshProUGUI score;
+	public TextMeshProUGUI lives;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(score.text);
+		Debug.Log(lives.text);
+	}
+}
