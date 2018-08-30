3cc979649a6ff1d7142c4a8069cd61bc78e52e35
Facundo Gonzalez Fernandez <fgonzalez@itba.edu.ar>
2018-08-29 21:08:08 -0300

    Add Game over screen

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,6 +7,6 @@ public class Menu : MonoBehaviour {
 
     public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("SampleScene");
     }
 }
